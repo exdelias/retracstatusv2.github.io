490d11c815fec8e2691c5c7ef05536cface4c096
Javier <javier@bullrich.dev>
2024-07-31 12:27:58 +0200

    added class to obtain site status

---
+++ log/src/status.ts
@@ -0,0 +1,27 @@
+import { ActionLogger } from "./github/types";
+import axios, { AxiosError } from "axios";
+
+export class StatusChecker {
+    constructor(private readonly siteName:string,private readonly healthEndpoint:string,
+        private readonly logger: ActionLogger){
+
+        }
+
+        async verifyEndpoint() :Promise<boolean> {
+            try {
+            const request = await axios.get(this.healthEndpoint);
+            this.logger.debug(`Contacted ${this.healthEndpoint} with status ${request.status}` );
+            return request.status >= 200 && request.status < 300;
+            } catch(err:unknown | Error |AxiosError){
+                if (axios.isAxiosError(err)){
+                   const axErr = err as AxiosError;
+                   this.logger.error(`Failed to contact ${this.siteName} with status ${axErr.status}`);
+                   this.logger.debug(JSON.stringify(axErr.toJSON()));
+                } else {
+                    this.logger.error("Failed with unknown error");
+                    this.logger.debug(JSON.stringify(err));
+                }
+            }
+            return false;
+        }
+}
--- log/src/test/index.test.ts
+++ log/src/test/index.test.ts
@@ -1,3 +1,14 @@
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
-});
+import { StatusChecker } from "../status";
+
+const sites:Array<[string,string]> = [
+  ["GitHub", "https://github.com"],
+  ["Facebook", "https://facebook.com"],
+  ["Google", "https://google.com"]
+]
+
+for(const [site,url] of sites) {
+  test(`Test obtaining metrics from ${site}`, async () => {
+    const statusChecker = new StatusChecker(site, url, console);
+    await expect(statusChecker.verifyEndpoint()).resolves.toBe(true);
+  })
+}
