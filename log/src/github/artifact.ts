3c6e2c35824d4c5f4601117f69ac5ff09bfaa090
Javier <javier@bullrich.dev>
2024-07-31 11:42:20 +0200

    added class to download artifact

---
+++ log/src/github/artifact.ts
@@ -0,0 +1,59 @@
+import { GitHubClient, ActionLogger, Repo } from "./types";
+import {writeFile} from "fs/promises";
+import {execSync} from "child_process";
+const artifactName = "REPORT.json";
+
+export class ArtifactManager {
+    constructor(
+        private readonly api: GitHubClient,
+        private readonly logger: ActionLogger,
+      ) {}
+
+     async getPreviousArtifact(repo:Repo) {
+        const workflows = await this.api.rest.actions.listRepoWorkflows(repo);
+
+        const workflow = workflows.data.workflows.find(w => w.path.includes(process.env.WORKFLOW_FILENAME ?? ""));
+
+        if (!workflow) {
+            this.logger.error("No workflow file found");
+            return null;
+        }
+
+        const runs = await this.api.rest.actions.listWorkflowRuns({
+            ...repo,
+            workflow_id: workflow.id,
+            status: "success",
+            per_page: 1
+          });
+
+          if(runs.data.total_count === 0) {
+            this.logger.error("No runs detected. Is this the first run?");
+            return;
+          }
+
+          for(const run of runs.data.workflow_runs) {
+            const artifacts = await this.api.rest.actions.listWorkflowRunArtifacts({
+                ...repo,
+                run_id: run.id
+              });   
+
+              const artifact = artifacts.data.artifacts.find(artifact => artifact.name === artifactName);
+
+              if(artifact) {
+                const response = await this.api.rest.actions.downloadArtifact({
+                    ...repo,
+                    artifact_id:artifact.id,
+                    archive_format: "zip"
+                });
+                await writeFile(artifactName, Buffer.from(response.data as string));
+                execSync(`unzip -o ${artifactName} -d ./logs`);
+
+                this.logger.info("Artifact downloaded correctly");
+
+              }
+              return `./logs/${artifactName}`;
+          }
+
+        
+      }
+}
