577039ce3944ee6a3db3b1ce7b061edd665f2546
Javier Bullrich <javier@bullrich.dev>
2024-07-31 15:04:10 +0000

    added artifact upload feature

--- log/src/test/index.test.ts
+++ log/src/test/index.test.ts
@@ -1,14 +1,27 @@
+import { ArtifactManager } from "../github/artifact";
+import { GitHubClient } from "../github/types";
 import { StatusChecker } from "../status";
 
-const sites:Array<[string,string]> = [
+const sites: Array<[string, string]> = [
   ["GitHub", "https://github.com"],
   ["Facebook", "https://facebook.com"],
   ["Google", "https://google.com"]
 ]
 
-for(const [site,url] of sites) {
+for (const [site, url] of sites) {
   test(`Test obtaining metrics from ${site}`, async () => {
     const statusChecker = new StatusChecker(site, url, console);
     await expect(statusChecker.verifyEndpoint()).resolves.toBe(true);
   })
 }
+
+test("Write to doc", async () => {
+  const siteResult: Array<[string, boolean]> = [];
+
+  for (const [site, url] of sites) {
+    const statusChecker = new StatusChecker(site, url, console);
+    siteResult.push([site, await statusChecker.verifyEndpoint()]);
+  }
+  const am = new ArtifactManager(null as unknown as GitHubClient, console);
+  await am.generateArtifact(siteResult);
+})
